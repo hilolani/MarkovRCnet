d24e1950ea470db1bd3f93b12af9e18ce3deddc8
Hiroyuki Akama <hilolani@gmail.com>
2026-01-21 15:51:36 +0900

    added tests/test_mcl.py

---
+++ tests/test_mcl.py
@@ -0,0 +1,6 @@
+def test_mcl_with_dataset():
+    from markovrcnet.datasets import load_all_adjmats
+    from markovrcnet.mcl import mclprocess
+    mats = load_all_adjmats()
+    result = mclprocess(mats["karateclub"], 10)
+    print(result)
