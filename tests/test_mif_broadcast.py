05fe4dbb2b39e5ca5524618a9d03cca8f0478139
Hiroyuki Akama <hilolani@gmail.com>
2026-01-23 17:03:26 +0900

    A careless miss for import block was fixed.

--- tests/test_mif_broadcast.py
+++ tests/test_mif_broadcast.py
@@ -1,6 +1,6 @@
 def test_mif_broadcast_with_dataset():
     from markovrcnet.datasets import load_all_adjmats
-    from markovrcnet.mif import MiF
+    from markovrcnet.mif import *
     mats = load_all_adjmats()
     result0 = MiF_broadcast(mats["karateclub"], 3)
     result1 = MiF_broadcast(mats["karateclub"], 3, loop = 1)
